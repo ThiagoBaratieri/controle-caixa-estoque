d83a4fab8539bb81bdce87d03b0f4a68383ca7d0
Thiago Baratieri <thiagobaratieri971@gmail.com>
2024-08-07 03:47:48 -0300

    Atualização e criação da tabela 'produtos'

--- database.py
+++ database.py
@@ -9,14 +9,23 @@ def create_tables():
     cursor = conn.cursor()
 
     cursor.execute('''
+    CREATE TABLE IF NOT EXISTS produtos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        quantidade INTEGER NOT NULL,
+        preco REAL NOT NULL
+    )
+    ''')
+    
+    cursor.execute('''
     CREATE TABLE IF NOT EXISTS vendas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         produto_id INTEGER,
         quantidade INTEGER,
-        total INTEGER,
+        total REAL,
         data TEXT,
         FOREIGN KEY (produto_id) REFERENCES produtos (id)
-        )
+    )
     ''')
 
     conn.commit()
