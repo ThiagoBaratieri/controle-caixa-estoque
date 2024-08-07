98cbadb60021164dd4b0d7a5a54112b095de789e
Thiago Baratieri <thiagobaratieri971@gmail.com>
2024-08-07 03:43:50 -0300

    Criação da tabela 'vendas'

--- database.py
+++ database.py
@@ -3,3 +3,24 @@ import sqlite3
 def create_connection():
     conn = sqlite3.connect('data/estoque_caixa.db')
     return conn
+
+def create_tables():
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS vendas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        produto_id INTEGER,
+        quantidade INTEGER,
+        total INTEGER,
+        data TEXT,
+        FOREIGN KEY (produto_id) REFERENCES produtos (id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
