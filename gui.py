405349f9d731d53c5098ea1a2bc72a8766ca6981
Thiago Baratieri <thiagobaratieri971@gmail.com>
2024-08-08 02:01:47 -0300

    Criação da função para adicionar um produto

--- gui.py
+++ gui.py
@@ -1,3 +1,26 @@
 import tkinter as tk
 from tkinter import messagebox
 import sqlite3
+
+# Função para adicionar um produto
+def add_product():
+    nome = nome_entry.get()
+    quantidade = quantidade_entry.get()
+    preco = preco_entry.get()
+    
+    if nome and quantidade and preco:
+        conn = sqlite3.connect('data/estoque_caixa.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO produtos (nome, quantidade, preco) VALUES (?, ?, ?)", (nome, quantidade, preco))
+        conn.commit()
+        conn.close()
+        
+        nome_entry.delete(0, tk.END)
+        quantidade_entry.delete(0, tk.END)
+        preco_entry.delete(0, tk.END)
+        messagebox.showinfo("Sucesso", "Produto adicionado com sucesso!")
+    else:
+        messagebox.showerror("Erro", "Todos os campos devem ser preenchidos.")
+    
+
+
